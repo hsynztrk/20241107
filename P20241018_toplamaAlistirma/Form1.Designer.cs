c89fdcfc0880eb34923f22e5ae41664efdaa6048
hsynztrk <hsynztrk@gmail.com>
2024-11-07 10:27:49 +0300

    Add files via upload

---
+++ P20241018_toplamaAlistirma/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace P20241018_toplamaAlistirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblT1_1 = new System.Windows.Forms.Label();
+            this.lblT1_2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSonuc1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblT2_1 = new System.Windows.Forms.Label();
+            this.lblT2_2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSonuc2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblT3_1 = new System.Windows.Forms.Label();
+            this.lblT3_2 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtSonuc3 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.lblSonuc1 = new System.Windows.Forms.Label();
+            this.lblSonuc2 = new System.Windows.Forms.Label();
+            this.lblSonuc3 = new System.Windows.Forms.Label();
+            this.lblDogruSayisi = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblT1_1
+            // 
+            this.lblT1_1.AutoSize = true;
+            this.lblT1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT1_1.Location = new System.Drawing.Point(57, 24);
+            this.lblT1_1.Name = "lblT1_1";
+            this.lblT1_1.Size = new System.Drawing.Size(73, 31);
+            this.lblT1_1.TabIndex = 0;
+            this.lblT1_1.Text = "_ _ _";
+            this.lblT1_1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblT1_2
+            // 
+            this.lblT1_2.AutoSize = true;
+            this.lblT1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT1_2.Location = new System.Drawing.Point(79, 55);
+            this.lblT1_2.Name = "lblT1_2";
+            this.lblT1_2.Size = new System.Drawing.Size(51, 31);
+            this.lblT1_2.TabIndex = 0;
+            this.lblT1_2.Text = "_ _";
+            this.lblT1_2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(26, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "______";
+            // 
+            // txtSonuc1
+            // 
+            this.txtSonuc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc1.Location = new System.Drawing.Point(30, 120);
+            this.txtSonuc1.Name = "txtSonuc1";
+            this.txtSonuc1.Size = new System.Drawing.Size(100, 38);
+            this.txtSonuc1.TabIndex = 1;
+            this.txtSonuc1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(27, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "+";
+            // 
+            // lblT2_1
+            // 
+            this.lblT2_1.AutoSize = true;
+            this.lblT2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT2_1.Location = new System.Drawing.Point(192, 24);
+            this.lblT2_1.Name = "lblT2_1";
+            this.lblT2_1.Size = new System.Drawing.Size(73, 31);
+            this.lblT2_1.TabIndex = 0;
+            this.lblT2_1.Text = "_ _ _";
+            this.lblT2_1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblT2_2
+            // 
+            this.lblT2_2.AutoSize = true;
+            this.lblT2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT2_2.Location = new System.Drawing.Point(214, 55);
+            this.lblT2_2.Name = "lblT2_2";
+            this.lblT2_2.Size = new System.Drawing.Size(51, 31);
+            this.lblT2_2.TabIndex = 0;
+            this.lblT2_2.Text = "_ _";
+            this.lblT2_2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(161, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "______";
+            // 
+            // txtSonuc2
+            // 
+            this.txtSonuc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc2.Location = new System.Drawing.Point(165, 120);
+            this.txtSonuc2.Name = "txtSonuc2";
+            this.txtSonuc2.Size = new System.Drawing.Size(100, 38);
+            this.txtSonuc2.TabIndex = 1;
+            this.txtSonuc2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(162, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 25);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "+";
+            // 
+            // lblT3_1
+            // 
+            this.lblT3_1.AutoSize = true;
+            this.lblT3_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT3_1.Location = new System.Drawing.Point(317, 24);
+            this.lblT3_1.Name = "lblT3_1";
+            this.lblT3_1.Size = new System.Drawing.Size(73, 31);
+            this.lblT3_1.TabIndex = 0;
+            this.lblT3_1.Text = "_ _ _";
+            this.lblT3_1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblT3_2
+            // 
+            this.lblT3_2.AutoSize = true;
+            this.lblT3_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT3_2.Location = new System.Drawing.Point(339, 55);
+            this.lblT3_2.Name = "lblT3_2";
+            this.lblT3_2.Size = new System.Drawing.Size(51, 31);
+            this.lblT3_2.TabIndex = 0;
+            this.lblT3_2.Text = "_ _";
+            this.lblT3_2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(286, 86);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(104, 31);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "______";
+            // 
+            // txtSonuc3
+            // 
+            this.txtSonuc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc3.Location = new System.Drawing.Point(290, 120);
+            this.txtSonuc3.Name = "txtSonuc3";
+            this.txtSonuc3.Size = new System.Drawing.Size(100, 38);
+            this.txtSonuc3.TabIndex = 1;
+            this.txtSonuc3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(287, 86);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(24, 25);
+            this.label12.TabIndex = 2;
+            this.label12.Text = "+";
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(32, 216);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(360, 34);
+            this.btnKontrol.TabIndex = 3;
+            this.btnKontrol.Text = "Kontrol Et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // lblSonuc1
+            // 
+            this.lblSonuc1.AutoSize = true;
+            this.lblSonuc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc1.Location = new System.Drawing.Point(27, 177);
+            this.lblSonuc1.Name = "lblSonuc1";
+            this.lblSonuc1.Size = new System.Drawing.Size(72, 25);
+            this.lblSonuc1.TabIndex = 4;
+            this.lblSonuc1.Text = "..........";
+            // 
+            // lblSonuc2
+            // 
+            this.lblSonuc2.AutoSize = true;
+            this.lblSonuc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc2.Location = new System.Drawing.Point(162, 177);
+            this.lblSonuc2.Name = "lblSonuc2";
+            this.lblSonuc2.Size = new System.Drawing.Size(72, 25);
+            this.lblSonuc2.TabIndex = 4;
+            this.lblSonuc2.Text = "..........";
+            // 
+            // lblSonuc3
+            // 
+            this.lblSonuc3.AutoSize = true;
+            this.lblSonuc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc3.Location = new System.Drawing.Point(287, 177);
+            this.lblSonuc3.Name = "lblSonuc3";
+            this.lblSonuc3.Size = new System.Drawing.Size(72, 25);
+            this.lblSonuc3.TabIndex = 4;
+            this.lblSonuc3.Text = "..........";
+            // 
+            // lblDogruSayisi
+            // 
+            this.lblDogruSayisi.AutoSize = true;
+            this.lblDogruSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDogruSayisi.Location = new System.Drawing.Point(26, 303);
+            this.lblDogruSayisi.Name = "lblDogruSayisi";
+            this.lblDogruSayisi.Size = new System.Drawing.Size(288, 31);
+            this.lblDogruSayisi.TabIndex = 5;
+            this.lblDogruSayisi.Text = "Toplam Doğru Sayısı:0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Yeniden Soru Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDogruSayisi);
+            this.Controls.Add(this.lblSonuc3);
+            this.Controls.Add(this.lblSonuc2);
+            this.Controls.Add(this.lblSonuc1);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSonuc3);
+            this.Controls.Add(this.txtSonuc2);
+            this.Controls.Add(this.txtSonuc1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblT3_2);
+            this.Controls.Add(this.lblT2_2);
+            this.Controls.Add(this.lblT1_2);
+            this.Controls.Add(this.lblT3_1);
+            this.Controls.Add(this.lblT2_1);
+            this.Controls.Add(this.lblT1_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblT1_1;
+        private System.Windows.Forms.Label lblT1_2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSonuc1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblT2_1;
+        private System.Windows.Forms.Label lblT2_2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSonuc2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblT3_1;
+        private System.Windows.Forms.Label lblT3_2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtSonuc3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Label lblSonuc1;
+        private System.Windows.Forms.Label lblSonuc2;
+        private System.Windows.Forms.Label lblSonuc3;
+        private System.Windows.Forms.Label lblDogruSayisi;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ P20241018_toplamaAlistirma/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P20241018_toplamaAlistirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int s1_1, s1_2,s2_1,s2_2,s3_1,s3_2,sonuc1,sonuc2,sonuc3;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblT1_1.Text = rnd.Next(100, 999).ToString();
+            lblT1_2.Text = rnd.Next(10, 99).ToString();
+            lblT2_1.Text = rnd.Next(100, 999).ToString();
+            lblT2_2.Text = rnd.Next(10, 99).ToString();
+            lblT3_1.Text = rnd.Next(100, 999).ToString();
+            lblT3_2.Text = rnd.Next(10, 99).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblT1_1.Text = rnd.Next(100,999).ToString();
+            lblT1_2.Text = rnd.Next(10,99).ToString();
+            lblT2_1.Text = rnd.Next(100,999).ToString();
+            lblT2_2.Text = rnd.Next(10,99).ToString();
+            lblT3_1.Text = rnd.Next(100,999).ToString();
+            lblT3_2.Text = rnd.Next(10,99).ToString();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            int dsayisi = 0;
+            s1_1 = Convert.ToInt32(lblT1_1.Text);
+            s1_2= Convert.ToInt32(lblT1_2.Text);
+            s2_1= Convert.ToInt32(lblT2_1.Text);
+            s2_2= Convert.ToInt32(lblT2_2.Text);
+            s3_1= Convert.ToInt32(lblT3_1.Text);
+            s3_2= Convert.ToInt32(lblT3_2.Text);
+            sonuc1 = s1_1 + s1_2;
+            sonuc2 = s2_1 + s2_2;
+            sonuc3 = s3_1 + s3_2;
+            int cevap1 = Convert.ToInt32(txtSonuc1.Text);
+            int cevap2 = Convert.ToInt32(txtSonuc2.Text);
+            int cevap3 = Convert.ToInt32(txtSonuc3.Text);
+            lblSonuc1.Text = (sonuc1==cevap1).ToString();
+            lblSonuc2.Text = (sonuc2==cevap2).ToString();
+            lblSonuc3.Text = (sonuc3==cevap3).ToString();
+            if (Convert.ToBoolean(lblSonuc1.Text))
+                dsayisi++;            
+            if (Convert.ToBoolean(lblSonuc2.Text))
+                dsayisi++;            
+            if (Convert.ToBoolean(lblSonuc3.Text))
+                dsayisi++;
+            lblDogruSayisi.Text = "Toplam Doğru Sayısı:" + dsayisi;
+        }
+    }
+}
